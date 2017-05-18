d09247479ad17e79857359b898c0d131742c004b
lylayueting <yueting_li@outlook.com>
2017-05-17 21:00:52 -0400

    More traversal algos

--- src/main/java/com/github/lyt/dsnalgo/datastructure/BinarySearchTree.java
+++ src/main/java/com/github/lyt/dsnalgo/datastructure/BinarySearchTree.java
@@ -109,7 +109,166 @@ public class BinarySearchTree {
   }
 
   public void delete(int value) {
+    this.root = deleteRecursive(this.root, value);
+    this.size--;
+  }
 
+  public int height() {
+    if (root == null) {
+      return 0;
+    }
+
+    Stack<BinaryTreeNode> stack = new Stack<>();
+    stack.push(root);
+    int height = root.getDept();
+
+    while (!stack.isEmpty()) {
+      BinaryTreeNode top = stack.peek();
+
+      if (top.getLeft() != null) {
+        stack.push(top.getLeft());
+        continue;
+      }
+
+      while (top.getRight() == null) {
+        height = height > top.getDept() ? height : top.getDept();
+        stack.pop();
+        if (stack.isEmpty()) {
+          return height;
+        }
+        top = stack.peek();
+      }
+
+      height = height > top.getDept() ? height : top.getDept();
+      stack.pop();
+      stack.push(top.getRight());
+    }
+
+    return height;
+  }
+
+  private static int biggest(BinaryTreeNode root) {
+    assert root != null;
+
+    while (root.getRight() != null) {
+      root = root.getRight();
+    }
+
+    return root.getValue();
+  }
+
+  private static int smallest(BinaryTreeNode root) {
+    assert root != null;
+
+    while (root.getLeft() != null) {
+      root = root.getLeft();
+    }
+
+    return root.getValue();
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (object == null || !(object instanceof BinarySearchTree)) {
+      return false;
+    }
+
+    BinarySearchTree other = (BinarySearchTree) object;
+    return inorderIterative(this.root).equals(inorderIterative(other.getRoot()));
+  }
+
+  public static void print(BinarySearchTree tree) {
+    if (tree.getRoot() == null) {
+      return;
+    }
+
+    Queue<BinaryTreeNode> parent = new ArrayDeque<>();
+    Queue<BinaryTreeNode> children = new ArrayDeque<>();
+
+    parent.add(tree.getRoot());
+
+    while (!parent.isEmpty()) {
+      BinaryTreeNode current = parent.remove();
+
+      System.out.print(current.toString());
+
+      if (null != current.getLeft()) {
+        children.add(current.getLeft());
+      }
+
+      if (null != current.getRight()) {
+        children.add(current.getRight());
+      }
+
+      if (parent.isEmpty()) {
+        parent.addAll(children);
+        children.clear();
+
+        System.out.print("|\n");
+      }
+    }
+  }
+
+  public static void printLevelOrder(BinarySearchTree tree) {
+    if (tree == null) {
+      return;
+    }
+
+    for (int i = 0; i <= tree.height(); i++) {
+      printLevelRecursive(tree.getRoot(), i);
+    }
+  }
+
+  private static void printLevelRecursive(BinaryTreeNode root, int depth) {
+    if (root == null) {
+      return;
+    }
+
+    if (depth == 0) {
+      System.out.print(root.toString());
+      return;
+    }
+
+    printLevelRecursive(root.getLeft(), depth - 1);
+    printLevelRecursive(root.getRight(), depth - 1);
+  }
+
+  public static BinaryTreeNode deleteRecursive(BinaryTreeNode root, int value) {
+    if (root == null) {
+      return null;
+    }
+
+    if (root.getValue() > value) {
+      root.setLeft(deleteRecursive(root.getLeft(), value));
+      return root;
+    }
+
+    if (root.getValue() < value) {
+      root.setRight(deleteRecursive(root.getRight(), value));
+      return root;
+    }
+
+    return deleteNodeHelper(root);
+  }
+
+  private static BinaryTreeNode deleteNodeHelper(BinaryTreeNode node) {
+    assert node != null;
+
+    if (node.getLeft() == null && node.getRight() == null) {
+      return null;
+    }
+
+    if (node.getLeft() != null) {
+      int nextSmaller = biggest(node.getLeft());
+      node.setValue(nextSmaller);
+      node.setLeft(deleteRecursive(node.getLeft(), nextSmaller));
+      return node;
+    }
+
+    int nextBigger = smallest(node.getRight());
+    node.setValue(nextBigger);
+    node.setRight(deleteRecursive(node.getRight(), nextBigger));
+    return node;
   }
 
   public static void printInorderRecursive(BinaryTreeNode root) {
@@ -124,7 +283,6 @@ public class BinarySearchTree {
 
   /**
    * Prints a BST with given root in a line separated by single space, pre-order.
-   * @param root
    */
   public static String preorderIterative(BinaryTreeNode root) {
     if (root == null) {
@@ -191,38 +349,129 @@ public class BinarySearchTree {
   }
 
   public static String postorderIterative(BinaryTreeNode root) {
+    if (root == null) {
+      return "";
+    }
 
+    Stack<BinaryTreeNode> reverseTraversal = new Stack<>();
+    reverseTraversal.push(root);
+    // For algorithm's sake, could use string reverse print directly.
+    Stack<BinaryTreeNode> visited = new Stack<>();
+    StringBuilder builder = new StringBuilder();
+
+    while (!reverseTraversal.isEmpty()) {
+      BinaryTreeNode top = reverseTraversal.pop();
+
+      if (top.getLeft() != null) {
+        reverseTraversal.push(top.getLeft());
+      }
+
+      if (top.getRight() != null) {
+        reverseTraversal.push(top.getRight());
+      }
+
+      visited.push(top);
+    }
+
+    while (!visited.isEmpty()) {
+      builder.append(visited.pop().getValue()).append(" ");
+    }
+
+    return builder.toString();
   }
 
-  public static void print(BinarySearchTree tree) {
-    if (tree.getRoot() == null) {
-      return;
+  public static String postorderIterativeOneStack(BinaryTreeNode root) {
+    if (root == null) {
+      return "";
     }
 
-    Queue<BinaryTreeNode> parent = new ArrayDeque<>();
-    Queue<BinaryTreeNode> children = new ArrayDeque<>();
+    Stack<BinaryTreeNode> stack = new Stack<>();
+    BinaryTreeNode currentRoot = root;
+    StringBuilder builder = new StringBuilder();
 
-    parent.add(tree.getRoot());
-
-    while (!parent.isEmpty()) {
-      BinaryTreeNode current = parent.remove();
-
-      System.out.print(current.toString());
-
-      if (null != current.getLeft()) {
-        children.add(current.getLeft());
+    do {
+      while (currentRoot != null) {
+        if (currentRoot.getRight() != null) {
+          stack.push(currentRoot.getRight());
+        }
+        stack.push(currentRoot);
+        currentRoot = currentRoot.getLeft();
       }
 
-      if (null != current.getRight()) {
-        children.add(current.getRight());
+      BinaryTreeNode top = stack.pop();
+
+      // top node has right child and HAVEN'T BEEN PROCESSED.
+      if (top.getRight() != null && !stack.isEmpty() && top.getRight() == stack.peek()) {
+        stack.pop(); // Take right node out of stack and process subtree.
+        stack.push(top);
+        currentRoot = top.getRight();
+        continue;
       }
 
-      if (parent.isEmpty()) {
-        parent.addAll(children);
-        children.clear();
+      builder.append(top.getValue()).append(" ");
+      currentRoot = null;
+    } while (!stack.isEmpty());
 
-        System.out.print("|\n");
+    return builder.toString();
+  }
+
+  public static String bfs(BinaryTreeNode root) {
+    if (root == null) {
+      return "";
+    }
+
+    Queue<BinaryTreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+
+    StringBuilder builder = new StringBuilder();
+
+    while (!queue.isEmpty()) {
+      BinaryTreeNode top = queue.remove();
+      builder.append(top.getValue()).append(" ");
+
+      if (top.getLeft() != null) {
+        queue.add(top.getLeft());
+      }
+
+      if (top.getRight() != null) {
+        queue.add(top.getRight());
       }
     }
+
+    return builder.toString();
+  }
+
+  public static String inorderWithoutStack(BinaryTreeNode root) {
+    if (root == null) {
+      return "";
+    }
+
+    StringBuilder builder = new StringBuilder();
+    BinaryTreeNode current = root;
+
+    while (current != null) {
+      if (current.getLeft() == null) {
+        builder.append(current.getValue()).append(" ");
+        current = current.getRight();
+        continue;
+      }
+
+      BinaryTreeNode prev = current.getLeft();
+
+      while (prev.getRight() != null && prev.getRight() != current) {
+        prev = prev.getRight();
+      }
+
+      if (prev.getRight() == null) {
+        prev.setRight(current);
+        current = current.getLeft();
+      } else {
+        builder.append(current.getValue()).append(" ");
+        prev.setRight(null);
+        current = current.getRight();
+      }
+    }
+
+    return builder.toString();
   }
 }
--- src/test/java/com/github/lyt/dsnalgo/datastructure/BinarySearchTreeTest.java
+++ src/test/java/com/github/lyt/dsnalgo/datastructure/BinarySearchTreeTest.java
@@ -8,6 +8,12 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 @RunWith(BlockJUnit4ClassRunner.class)
 public class BinarySearchTreeTest {
 
+  private static final int[] PERFECT_BST_3 = {4, 2, 6, 1, 3, 5, 7};
+
+  private static final String PERFECT_BST_3_INORDER = "1 2 3 4 5 6 7 ";
+  private static final String PERFECT_BST_3_PREORDER = "4 2 1 3 6 5 7 ";
+  private static final String PERFECT_BST_3_POSTORDER = "1 3 2 5 7 6 4 ";
+
 	@Test
 	public void testRun() {
 		int[] input = {4, 2, 6, 1, 3, 5, 7};
@@ -17,7 +23,9 @@ public class BinarySearchTreeTest {
 		BinarySearchTree.print(bst);
 
     BinarySearchTree.printInorderRecursive(bst.getRoot());
-	}
+
+    BinarySearchTree.printLevelOrder(bst);
+  }
 
 	@Test
   public void testPreorderIterative() {
@@ -41,5 +49,31 @@ public class BinarySearchTreeTest {
     String expected = "1 2 3 4 5 6 7 8 ";
 
     Assert.assertEquals(expected, BinarySearchTree.inorderIterative(bst.getRoot()));
+    Assert.assertEquals(expected, BinarySearchTree.inorderWithoutStack(bst.getRoot()));
+  }
+
+  @Test
+  public void testPostorderIterative() {
+    int[] input = {7, 3, 8, 2, 6, 1, 4, 5};
+
+    BinarySearchTree bst = new BinarySearchTree(input);
+    BinarySearchTree.print(bst);
+
+    String expected = "1 2 5 4 6 3 8 7 ";
+
+    Assert.assertEquals(expected, BinarySearchTree.postorderIterative(bst.getRoot()));
+    Assert.assertEquals(expected, BinarySearchTree.postorderIterativeOneStack(bst.getRoot()));
+  }
+
+  @Test
+  public void testBFS() {
+    int[] input = {7, 3, 8, 2, 6, 1, 4, 5};
+
+    BinarySearchTree bst = new BinarySearchTree(input);
+    BinarySearchTree.print(bst);
+
+    String expected = "7 3 8 2 6 1 4 5 ";
+
+    Assert.assertEquals(expected, BinarySearchTree.bfs(bst.getRoot()));
   }
 }
